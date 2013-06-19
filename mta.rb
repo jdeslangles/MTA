3db5a8e4de2c4d299256dacac2d3d7754e2b71fb
jdeslangles <julien.deslangles@gmail.com>
2013-06-19 17:59:10 +0100

    Working build of menu

--- mta.rb
+++ mta.rb
@@ -9,32 +9,45 @@ end
 
 menu
 
-LINE_L = { "Times Square": 1, "34th": 2, "28th": 3, "23rd": 4, "Union Square": 5, "8th": 6}
-LINE_N = {}
-LINE_6 = {}
+LINE_L = { times_square: "1", st34: "2",st28: "3",st23: "4",union_square: "5",st8: "6"}
+LINE_N = { st8: 1, st6: 2, union_square: 3, st3: 4, st1: 5 }
+
+def stops
 
 puts "What train would you like to get on:"
 puts "Line (L), line (N) or line (6) ?"
 line_on = gets.chomp.upcase
 
-puts "Which stop are you getting on?"
-puts "stop(1): Times Square"
-puts "stop(2): 34th"
-puts "stop(3): 28th"
-puts "stop(4): 23rd"
-puts "stop(5): Union Square"
-puts "stop(6): 8th"
-stop_on = gets.chomp
+  case line_on
+    when "L"
+    puts "Which stop are you getting on?"
+    puts "stop(1): Times Square"
+    puts "stop(2): 34th"
+    puts "stop(3): 28th"
+    puts "stop(4): 23rd"
+    puts "stop(5): Union Square"
+    puts "stop(6): 8th"
+    stop_on = gets.chomp
 
-puts "What train would you like to get off:"
-puts "Line (L), line (N) or line (6) ?"
-line_off = gets.chomp.upcase
+  when "N"
 
-puts "Which stop are you getting off?"
-puts "stop(1): Times Square"
-puts "stop(2): 34th"
-puts "stop(3): 28th"
-puts "stop(4): 23rd"
-puts "stop(5): Union Square"
-puts "stop(6): 8th"
-stop_off = gets.chomp
+
+    puts "What train would you like to get off:"
+    puts "Line (L), line (N) or line (6) ?"
+    line_off = gets.chomp.upcase
+  end
+
+  case line_off
+  when "L"
+    puts "Which stop are you getting off?"
+    puts "stop(1): Times Square"
+    puts "stop(2): 34th"
+    puts "stop(3): 28th"
+    puts "stop(4): 23rd"
+    puts "stop(5): Union Square"
+    puts "stop(6): 8th"
+    stop_off = gets.chomp
+  end
+end
+
+stops
